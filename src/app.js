dca5df5602b3ec4b9cff25f45fdc106d334a4969
Gabo <gaboramirezdev@gmail.com>
2025-01-29 18:58:16 -0600

    Adding form and functionality

--- src/app.js
+++ src/app.js
@@ -5,10 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
+let who_array = JSON.parse(localStorage.getItem("who")) || [
+  "The dog",
+  "My grandma",
+  "The mailman",
+  "My bird"
+];
+let action_array = JSON.parse(localStorage.getItem("action")) || [
+  "ate",
+  "peed",
+  "crushed",
+  "broke"
+];
+let what_array = JSON.parse(localStorage.getItem("what")) || [
+  "my homework",
+  "my phone",
+  "the car"
+];
+let when_array = JSON.parse(localStorage.getItem("when")) || [
   "before the class",
   "when I was sleeping",
   "while I was exercising",
@@ -18,6 +32,8 @@ let when = [
 
 const button = document.getElementById("generate-btn");
 const excuseText = document.getElementById("excuse");
+
+const sbm_btn = document.querySelector("#submit_btn");
 
 function generarNumeroAleatorioEntero(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -35,12 +51,40 @@ window.onload = function() {
     let num_what = generarNumeroAleatorioEntero(0, 2);
 
     let excuse_text = [
-      who[num_random[0]],
-      action[num_random[1]],
-      what[num_what],
-      when[num_random[2]]
+      who_array[num_random[0]],
+      action_array[num_random[1]],
+      what_array[num_what],
+      when_array[num_random[2]]
     ].join(" ");
     excuseText.innerText = excuse_text;
+
+    //Checks that the localStorage is being added to the arrays
+    // console.log(who_array);
   });
-  console.log("Excuse generator loaded successfully!");
+
+  sbm_btn.addEventListener("click", e => {
+    e.preventDefault();
+    const who = document.querySelector("#who").value;
+    const action = document.querySelector("#action").value;
+    const what = document.querySelector("#what").value;
+    const when = document.querySelector("#when").value;
+
+    if (who === "" || action === "" || what === "" || when === "") {
+      alert("You must enter info to all of the entries");
+    }
+
+    who_array.push(who);
+    action_array.push(action);
+    what_array.push(what);
+    when_array.push(when);
+
+    excuseText.innerText = `${who} ${action} ${what} ${when}`;
+
+    document.querySelector("#formulario").reset();
+
+    localStorage.setItem("who", JSON.stringify(who_array));
+    localStorage.setItem("action", JSON.stringify(action_array));
+    localStorage.setItem("what", JSON.stringify(what_array));
+    localStorage.setItem("when", JSON.stringify(when_array));
+  });
 };
